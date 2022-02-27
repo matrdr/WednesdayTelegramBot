a7658e2c0435f77f30437b2cd0f79d2113f3db01
Matthias Rader <matthias.rader@outlook.com>
2022-02-27 20:41:46 +0100

    Port to Telegram.Bot API v17

--- Program.cs
+++ Program.cs
@@ -1,139 +1,180 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
-using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Extensions.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace BotTest
 {
-    class Program
-    {
-        static TelegramBotClient botClient;
+	class Program
+	{
+		static TelegramBotClient botClient;
 
-        static void Main()
-        {
-            botClient = new TelegramBotClient(
-                /* INSERT YOUR BOT-TOKEN HERE */"asdf"
-            );
-            Subscribers.ReadSubscribersFromFile();
+		async static Task Main()
+		{
+			botClient = new TelegramBotClient(
+				"INSERT BOT TOKEN HERE"
+			);
+			Subscribers.ReadSubscribersFromFile();
 
-            var wednesdayInvestigator = new WednesdayInvestigator();
+			var wednesdayInvestigator = new WednesdayInvestigator();
 
-            wednesdayInvestigator.ItIsWednesdayEvent += Bot_OnWednesday;
-            wednesdayInvestigator.BeginInvestigation();
+			wednesdayInvestigator.ItIsWednesdayEvent += Bot_OnWednesday;
+			wednesdayInvestigator.BeginInvestigation();
 
-            var me = botClient.GetMeAsync().Result;
-            Console.WriteLine(
-                $"Hello, World! I am user {me.Id}" +
-                $"and my name is {me.FirstName}."
-            );
+			var me = await botClient.GetMeAsync();
+			Console.WriteLine(
+				$"Hello, World! I am user {me.Id}" +
+				$"and my name is {me.FirstName}."
+			);
 
-            botClient.OnMessage += Bot_OnMessage;
-            botClient.StartReceiving()
+			var receiverOptions = new ReceiverOptions
+			{
+				// receive all update types
+				AllowedUpdates = { }
+			};
 
-            // Bot Command Menu
-            uint input = 0U;
-            do
-            {
-                Console.WriteLine("0...Exit");
-                Console.WriteLine("1...Show Subscribers");
-                Console.WriteLine("2...Send Message");
-                Console.WriteLine("3...Remove All Subscribers");
-                Console.WriteLine("4...Time until Wednesday");
-                input = uint.Parse(Console.ReadLine());
+			using var cts = new CancellationTokenSource();
+			botClient.StartReceiving(
+				HandleUpdateAsync,
+				HandleError,
+				receiverOptions,
+				cts.Token);
 
-                switch (input)
-                {
-                    case 0U:
-                        break;
-                    case 1U:
-                        Subscribers.PrintSubscribers();
-                        break;
-                    case 2U:
-                        Console.Write("ChatID: ");
-                        ChatId id = Console.ReadLine();
-                        Console.WriteLine("Message: ");
-                        string text = Console.ReadLine();
-                        botClient.SendTextMessageAsync(id, text);
-                        break;
-                    case 3U:
-                        previousSubscriberOperation.Wait();
-                        previousSubscriberOperation = Subscribers.RemoveAll();
-                        break;
-                    case 4U:
-                        Console.WriteLine(
-                            WednesdayInvestigator.WhenIsWednesdayMyDude()
-                        );
-                        break;
-                }
+			// Bot Command Menu
+			uint input = 0U;
+			do
+			{
+				Console.WriteLine("0...Exit");
+				Console.WriteLine("1...Show Subscribers");
+				Console.WriteLine("2...Send Message");
+				Console.WriteLine("3...Remove All Subscribers");
+				Console.WriteLine("4...Time until Wednesday");
+				input = uint.Parse(Console.ReadLine());
 
-            } while (input != 0U);
+				switch (input)
+				{
+					case 0U:
+						break;
+					case 1U:
+						Subscribers.PrintSubscribers();
+						break;
+					case 2U:
+						Console.Write("ChatID: ");
+						var id = Console.ReadLine();
+						Console.WriteLine("Message: ");
+						var text = Console.ReadLine();
+						await botClient.SendTextMessageAsync(id, text);
+						break;
+					case 3U:
+						previousSubscriberOperation.Wait();
+						previousSubscriberOperation = Subscribers.RemoveAll();
+						break;
+					case 4U:
+						Console.WriteLine(
+							WednesdayInvestigator.WhenIsWednesdayMyDude()
+						);
+						break;
+				}
 
-            previousSubscriberOperation.Wait();
-            wednesdayInvestigator.EndInvestigation();
-        }
+			} while (input != 0U);
 
-        static void Bot_OnMessage(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text != null)
-            {
-                // Es handelt sich um eine Textnachricht
-                Console.WriteLine($"Received a text message in chat " +
-                    $"{e.Message.Chat.Id}. The Message is {e.Message.Text}");
+			cts.Cancel();
+			await previousSubscriberOperation;
+			wednesdayInvestigator.EndInvestigation();
+		}
 
-                if (e.Message.Text.StartsWith("/"))
-                {
-                    // Es wird ein Kommando erwartet
-                    switch (e.Message.Text)
-                    {
-                        case "/start":
-                            previousSubscriberOperation.Wait();
-                            previousSubscriberOperation =
-                                Subscribers.AddSubscriber(e.Message.Chat.Id);
-                            break;
-                        case "/unsubscribe":
-                            previousSubscriberOperation.Wait();
-                            previousSubscriberOperation =
-                                Subscribers.RemoveSubscriber(e.Message.Chat.Id);
-                            break;
-                        case "/whensdaymydude":
-                            botClient
-                                .SendTextMessageAsync(
-                                    e.Message.Chat,
-                                    WednesdayInvestigator
-                                        .WhenIsWednesdayMyDude()
-                                        .ToString())
-                                .Wait();
-                            break;
-                    }
-                }
-                else
-                {
-                    botClient.SendTextMessageAsync(
-                      chatId: e.Message.Chat,
-                      text: "You said:\n" + e.Message.Text
-                    ).Wait();
-                }
-            }
-        }
+		private static async Task HandleUpdateAsync(
+			ITelegramBotClient client,
+			Update update,
+			CancellationToken cancellationToken)
+		{
+			if (update.Type == UpdateType.Message &&
+				update.Message!.Type == MessageType.Text)
+			{
+				var chatId = update.Message.Chat.Id;
+				var messageText = update.Message.Text;
 
-        static void Bot_OnWednesday()
-        {
-            var sendTasks = new List<Task<Message>>();
-            foreach (ChatId id in Subscribers.ChatIDs)
-            {
-                sendTasks.Add(
-                    botClient.SendTextMessageAsync(
-                        id,
-                        "IT IS WEDNESDAY, MY DUDE!"
-                    )
-                );
-            }
+				Console.WriteLine($"Received text message: {messageText}");
+				await HandleTextMessage(messageText, chatId);
+			}
+		}
 
-            Task.WaitAll(sendTasks.ToArray());
-        }
+		private static async Task HandleTextMessage(string messageText, long chatId)
+		{
+			// Es handelt sich um eine Textnachricht
+			if (messageText.StartsWith("/"))
+			{
+				// Es wird ein Kommando erwartet
+				switch (messageText)
+				{
+					case "/start":
+					case "/subscribe":
+						await previousSubscriberOperation;
+						previousSubscriberOperation =
+							Subscribers.AddSubscriber(chatId);
+						break;
+					case "/unsubscribe":
+						await previousSubscriberOperation;
+						previousSubscriberOperation =
+							Subscribers.RemoveSubscriber(chatId);
+						break;
+					case "/whensdaymydude":
+						await botClient.SendTextMessageAsync(
+							chatId,
+							WednesdayInvestigator
+								.WhenIsWednesdayMyDude()
+								.ToString());
+						break;
+				}
+			}
+			else
+			{
+				await botClient.SendTextMessageAsync(
+				  chatId,
+				  $"It is {messageText}, my dude!"
+				);
+			}
+		}
 
-        private static Task previousSubscriberOperation = Task.CompletedTask;
-    }
+		private static Task HandleError(
+			ITelegramBotClient botClient,
+			Exception exception,
+			CancellationToken cancellation)
+		{
+			var ErrorMessage = exception switch
+			{
+				ApiRequestException apiRequestException
+					=> $"Telegram API Error:\n" +
+				       $"[{apiRequestException.ErrorCode}]" +
+					   $"\n{apiRequestException.Message}",
+				_ => exception.ToString()
+			};
+
+			Console.Error.WriteLine(ErrorMessage);
+			return Task.CompletedTask;
+		}
+
+		private static void Bot_OnWednesday()
+		{
+			var sendTasks = new List<Task<Message>>();
+			foreach (var id in Subscribers.ChatIDs)
+			{
+				sendTasks.Add(
+					botClient.SendTextMessageAsync(
+						id,
+						"IT IS WEDNESDAY, MY DUDE!"
+					)
+				);
+			}
+
+			Task.WaitAll(sendTasks.ToArray());
+		}
+
+		private static Task previousSubscriberOperation = Task.CompletedTask;
+	}
 }
